72af19e51ea4d3169cd27d262c30d4c89ef66129
jiyoungnim <cloundyon31@gmail.com>
2025-02-26 22:12:27 +0900

    📝 290. Using the "useSWR" NextJS Hook(Loading...만 출력되는 문제 발생, 미해결..)

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,39 +1,68 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 function LastSalesPage() {
   const [sales, setSales] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
+
+  const { data, error } = useSWR(
+    "https://nextjs-course-7bf07-default-rtdb.firebaseio.com/sales.json"
+  );
 
   useEffect(() => {
-    setIsLoading(true);
-    fetch("https://nextjs-course-7bf07-default-rtdb.firebaseio.com/sales.json")
-      .then((response) => response.json())
-      .then((data) => {
-        const transformedSales = [];
+    if (data) {
+      const transformedSales = [];
 
-        for (const key in data) {
-          transformedSales.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
-        setSales(transformedSales);
-        setIsLoading(false);
-      });
-  }, []);
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
 
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
+      setSales(transformedSales);
+    }
+  }, [data]);
+
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   fetch("https://nextjs-course-7bf07-default-rtdb.firebaseio.com/sales.json")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const transformedSales = [];
+
+  //       for (const key in data) {
+  //         transformedSales.push({
+  //           id: key,
+  //           username: data[key].username,
+  //           volume: data[key].volume,
+  //         });
+  //       }
+  //       setSales(transformedSales);
+  //       setIsLoading(false);
+  //     });
+  // }, []);
+
+  // if (isLoading) {
+  //   return <p>Loading...</p>;
+  // }
 
   // if (!sales) {
   //   return <p>No Data yet</p>;
   // }
 
+  if (!data || !sales) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p> Failed to load.</p>;
+  }
+
   return (
     <ul>
-      {sales?.map((sale) => (
+      {sales.map((sale) => (
         <li key={sale.id}>
           {sale.username} - {sale.volume}
         </li>
