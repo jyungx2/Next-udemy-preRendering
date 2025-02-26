20cb7c616c37e67be795c70ef1e3c0cfa65cff84
jiyoungnim <cloundyon31@gmail.com>
2025-02-26 15:36:05 +0900

    📝 271. Utilizing Incremental Static Generation (ISR)

--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,7 @@ export async function getStaticProps() {
     props: {
       products: data.products,
     },
+    revalidate: 120, // 어떤 앱을 만들지에 따라 달라진다. (dev모드에서는 nodemon에 의해서 모든 요청에 대해 새로운 데이터를 업데이트해서 필요없지만, production모드에서는 중요한 코드!!)
   };
 }
 
