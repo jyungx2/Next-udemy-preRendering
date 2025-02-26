ffd3b8ff3086d766c824ea23284eed4a7bb07f6e
jiyoungnim <cloundyon31@gmail.com>
2025-02-26 17:21:21 +0900

    📝 276&277. Using " getStaticPaths" For Dynamic Pages

--- pages/[pid].js
+++ pages/[pid].js
@@ -29,4 +29,17 @@ export async function getStaticProps(context) {
   };
 }
 
+// Dynamic Page의 경우, 서버 측에서 얼마나 많은 양의 상세페이지가 로드될지 알 방법이 없기 때문에
+// 다음과 같은 getStaticPaths()없이 getStaticProps()만 사용해 데이터를 가져오면 에러가 발생한다.
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { pid: "p1" } },
+      { params: { pid: "p2" } },
+      { params: { pid: "p3" } },
+    ],
+    fallback: false,
+  };
+}
+
 export default ProductDetailPage;
